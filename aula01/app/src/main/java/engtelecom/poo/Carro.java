585ce6825727512239e6afd3b092d6406c27646b
Sergio-r1 <sergio-r1@outlook.com>
2023-03-08 17:23:27 -0300

    Commit 1 - Iniciando o projeto

---
+++ aula01/app/src/main/java/engtelecom/poo/Carro.java
@@ -0,0 +1,26 @@
+package engtelecom.poo;
+
+public class Carro {
+
+    //atributos da classe
+    private String modelo;
+    private int velocidadeAtual;
+
+    //metodos
+    public void definirModelo(String m){
+
+    }
+
+    public void definirVelocidadeAtual(int v){
+
+    }
+
+    public String obterModelo(){
+        return modelo;
+    }
+
+    public int obterVelocidadeAtual(){
+        return velocidadeAtual;
+    }
+
+}
